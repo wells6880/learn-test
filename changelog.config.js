65ad7bafd81f63dc601515079f9c610a2f9192f8
wangzhilin <sunheting@sunhetingdeMacBook-Pro.local>
2020-10-10 10:45:35 +0800

    test: test

--- changelog.config.js
+++ changelog.config.js
@@ -17,10 +17,10 @@ module.exports = {
         'type',
         'scope',
         'subject',
-        'body'
+        // 'body'
         // 'breaking'
         // 'issues',
-        // 'lerna'
+        'lerna'
     ],
     scopes: [],
     types: {
